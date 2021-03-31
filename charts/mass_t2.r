9f8622cc39ac05dd7513c826790974969ccadea5
Mathis Logemann <mathisloge@gmail.com>
2021-03-31 12:10:21 +0200

    mass

--- charts/mass_t2.r
+++ charts/mass_t2.r
@@ -1,9 +1,9 @@
 library(ggplot2)
-luarocks <- read.csv(file = 'mass_luarocks_t1.csv')
-koreader <- read.csv(file = 'mass_koreader_t1.csv')
-awesome <- read.csv(file = 'mass_awesome_t1.csv')
-luakit <- read.csv(file = 'mass_luakit_t1.csv')
-apisix <- read.csv(file = 'mass_apisix_t1.csv')
+luarocks <- read.csv(file = 'mass_luarocks.csv')
+koreader <- read.csv(file = 'mass_koreader.csv')
+awesome <- read.csv(file = 'mass_awesome.csv')
+luakit <- read.csv(file = 'mass_luakit.csv')
+apisix <- read.csv(file = 'mass_apisix.csv')
 projectfile <- rbind(koreader, luarocks)
 projectfile <- rbind(projectfile, awesome)
 projectfile <- rbind(projectfile, luakit)
@@ -14,4 +14,4 @@ plot_clones_loc <- ggplot(projectfile, aes(x = mass, y = occurrences, fill=proje
     geom_line(aes(color=project)) +
     labs(x = "Masse", y = "Vorkommen")
 plot_clones_loc$labels$colour <- "Projekt"
-ggsave(plot_clones_loc, file="plot_mass_t1.pdf", dpi=600)
+ggsave(plot_clones_loc, file="plot_mass_t2.pdf", dpi=600)
